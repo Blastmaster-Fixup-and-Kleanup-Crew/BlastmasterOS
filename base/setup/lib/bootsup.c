e94c7d5b89986fc40435df1ed5327d1fabe82ea4
Blastmaster Fixup & Kleanup Crew <191918158+Blastmaster-Fixup-and-Kleanup-Crew@users.noreply.github.com>
2026-09-19 16:03:25 -0400

    Rename ReactOS to BlastmasterOS in bootsup.c

--- base/setup/lib/bootsup.c
+++ base/setup/lib/bootsup.c
@@ -73,59 +73,59 @@ CreateFreeLoaderReactOSEntries(
 
     Options->OsLoadPath = ArcPath;
 
-    /* ReactOS */
+    /* BlastmasterOS */
     // BootEntry->BootEntryKey = MAKESTRKEY(L"ReactOS");
-    BootEntry->FriendlyName = L"\"ReactOS\"";
+    BootEntry->FriendlyName = L"\"BlastmasterOS\"";
     Options->OsLoadOptions  = L"/FASTDETECT";
-    AddBootStoreEntry(BootStoreHandle, BootEntry, MAKESTRKEY(L"ReactOS"));
+    AddBootStoreEntry(BootStoreHandle, BootEntry, MAKESTRKEY(L"BlastmasterOS"));
 
     /* ReactOS_Debug */
     // BootEntry->BootEntryKey = MAKESTRKEY(L"ReactOS_Debug");
-    BootEntry->FriendlyName = L"\"ReactOS (Debug)\"";
+    BootEntry->FriendlyName = L"\"BlastmasterOS (Debug)\"";
     Options->OsLoadOptions  = L"/DEBUG /DEBUGPORT=COM1 /BAUDRATE=115200 /SOS";
-    AddBootStoreEntry(BootStoreHandle, BootEntry, MAKESTRKEY(L"ReactOS_Debug"));
+    AddBootStoreEntry(BootStoreHandle, BootEntry, MAKESTRKEY(L"BlastmasterOS_Debug"));
 
 #ifdef _WINKD_
-    /* ReactOS_VBoxDebug */
-    // BootEntry->BootEntryKey = MAKESTRKEY(L"ReactOS_VBoxDebug");
+    /* BlastmasterOS_VBoxDebug */
+    // BootEntry->BootEntryKey = MAKESTRKEY(L"BlastmasterOS_VBoxDebug");
     BootEntry->FriendlyName = L"\"ReactOS (VBox Debug)\"";
     Options->OsLoadOptions  = L"/DEBUG /DEBUGPORT=VBOX /SOS";
-    AddBootStoreEntry(BootStoreHandle, BootEntry, MAKESTRKEY(L"ReactOS_VBoxDebug"));
+    AddBootStoreEntry(BootStoreHandle, BootEntry, MAKESTRKEY(L"BlastmasterOS_VBoxDebug"));
 #endif
 #if DBG
 #ifndef _WINKD_
-    /* ReactOS_KdSerial */
-    // BootEntry->BootEntryKey = MAKESTRKEY(L"ReactOS_KdSerial");
-    BootEntry->FriendlyName = L"\"ReactOS (RosDbg)\"";
+    /* BlastmasterOS_KdSerial */
+    // BootEntry->BootEntryKey = MAKESTRKEY(L"BlastmasterOS_KdSerial");
+    BootEntry->FriendlyName = L"\"BlastmasterOS (RosDbg)\"";
     Options->OsLoadOptions  = L"/DEBUG /DEBUGPORT=COM1 /BAUDRATE=115200 /SOS /KDSERIAL";
-    AddBootStoreEntry(BootStoreHandle, BootEntry, MAKESTRKEY(L"ReactOS_KdSerial"));
+    AddBootStoreEntry(BootStoreHandle, BootEntry, MAKESTRKEY(L"BlastmasterOS_KdSerial"));
 #endif
 
-    /* ReactOS_Screen */
-    // BootEntry->BootEntryKey = MAKESTRKEY(L"ReactOS_Screen");
-    BootEntry->FriendlyName = L"\"ReactOS (Screen)\"";
+    /* BlastmasterOS_Screen */
+    // BootEntry->BootEntryKey = MAKESTRKEY(L"BlastmasterOS_Screen");
+    BootEntry->FriendlyName = L"\"BlastmasterOS (Screen)\"";
     Options->OsLoadOptions  = L"/DEBUG /DEBUGPORT=SCREEN /SOS";
-    AddBootStoreEntry(BootStoreHandle, BootEntry, MAKESTRKEY(L"ReactOS_Screen"));
+    AddBootStoreEntry(BootStoreHandle, BootEntry, MAKESTRKEY(L"BlastmasterOS_Screen"));
 
-    /* ReactOS_LogFile */
-    // BootEntry->BootEntryKey = MAKESTRKEY(L"ReactOS_LogFile");
-    BootEntry->FriendlyName = L"\"ReactOS (Log file)\"";
+    /* BlastmasterOS_LogFile */
+    // BootEntry->BootEntryKey = MAKESTRKEY(L"BlastmasterOS_LogFile");
+    BootEntry->FriendlyName = L"\"BlastmasterOS (Log file)\"";
     Options->OsLoadOptions  = L"/DEBUG /DEBUGPORT=FILE /SOS";
     AddBootStoreEntry(BootStoreHandle, BootEntry, MAKESTRKEY(L"ReactOS_LogFile"));
 
-    /* ReactOS_Ram */
-    // BootEntry->BootEntryKey = MAKESTRKEY(L"ReactOS_Ram");
-    BootEntry->FriendlyName = L"\"ReactOS (RAM Disk)\"";
-    Options->OsLoadPath     = L"ramdisk(0)\\ReactOS";
-    Options->OsLoadOptions  = L"/DEBUG /DEBUGPORT=COM1 /BAUDRATE=115200 /SOS /RDPATH=reactos.img /RDIMAGEOFFSET=32256";
-    AddBootStoreEntry(BootStoreHandle, BootEntry, MAKESTRKEY(L"ReactOS_Ram"));
+    /* BlastmasterOS_Ram */
+    // BootEntry->BootEntryKey = MAKESTRKEY(L"BlastmasterOS_Ram");
+    BootEntry->FriendlyName = L"\"BlastmasterOS (RAM Disk)\"";
+    Options->OsLoadPath     = L"ramdisk(0)\\BlastmasterOS";
+    Options->OsLoadOptions  = L"/DEBUG /DEBUGPORT=COM1 /BAUDRATE=115200 /SOS /RDPATH=blastmasteros.img /RDIMAGEOFFSET=32256";
+    AddBootStoreEntry(BootStoreHandle, BootEntry, MAKESTRKEY(L"BlastmasterOS_Ram"));
 
     /* ReactOS_EMS */
-    // BootEntry->BootEntryKey = MAKESTRKEY(L"ReactOS_EMS");
-    BootEntry->FriendlyName = L"\"ReactOS (Emergency Management Services)\"";
+    // BootEntry->BootEntryKey = MAKESTRKEY(L"BlastmasterOS_EMS");
+    BootEntry->FriendlyName = L"\"BlastmasterOS (Emergency Management Services)\"";
     Options->OsLoadPath     = ArcPath;
     Options->OsLoadOptions  = L"/DEBUG /DEBUGPORT=COM1 /BAUDRATE=115200 /SOS /redirect=com2 /redirectbaudrate=115200";
-    AddBootStoreEntry(BootStoreHandle, BootEntry, MAKESTRKEY(L"ReactOS_EMS"));
+    AddBootStoreEntry(BootStoreHandle, BootEntry, MAKESTRKEY(L"BlastmasterOS_EMS"));
 #endif
 
 
