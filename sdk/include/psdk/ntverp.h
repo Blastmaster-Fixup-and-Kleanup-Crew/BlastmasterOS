919466bb7b29a317b24555605db5cb596ea2c0ba
Blastmaster Fixup & Kleanup Crew <191918158+Blastmaster-Fixup-and-Kleanup-Crew@users.noreply.github.com>
2026-09-19 15:26:11 -0400

    Update Windows NT version and build numbers

--- sdk/include/psdk/ntverp.h
+++ sdk/include/psdk/ntverp.h
@@ -16,14 +16,14 @@
 //
 // Windows NT Build 3790.3959
 //
-#define VER_PRODUCTBUILD                    3790
-#define VER_PRODUCTBUILD_QFE                3959
+#define VER_PRODUCTBUILD                    67
+#define VER_PRODUCTBUILD_QFE                0
 
 //
 // Windows NT Version 5.2
 //
-#define VER_PRODUCTMAJORVERSION             5
-#define VER_PRODUCTMINORVERSION             2
+#define VER_PRODUCTMAJORVERSION             1
+#define VER_PRODUCTMINORVERSION             0
 #define VER_PRODUCTVERSION_W                (0x0502)
 #define VER_PRODUCTVERSION_DW               (0x05020000 | VER_PRODUCTBUILD)
 
