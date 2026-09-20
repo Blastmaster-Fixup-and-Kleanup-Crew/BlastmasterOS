0e4306702e385a96f9ffb359417181dd64339ac9
Blastmaster Fixup & Kleanup Crew <191918158+Blastmaster-Fixup-and-Kleanup-Crew@users.noreply.github.com>
2026-09-20 11:49:28 -0400

    Add resource definitions for Freecell game

---
+++ base/applications/games/freecell/resource.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define IDI_FREECELL       100
+
+#define IDR_MENU1          102
+#define IDR_ACCELERATOR1   103
+
+#define IDM_GAME_NEW       1001
+#define IDM_GAME_UNDO      1002
+#define IDM_GAME_EXIT      1003
+#define IDM_HELP_ABOUT     1004
+
+#define IDS_FREECELL_NAME  1100
+#define IDS_FREECELL_ABOUT 1101
+#define IDS_FREECELL_QUIT  1102
+#define IDS_FREECELL_WIN   1103
