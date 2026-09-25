baecd4daa788f737d8de116e625d3c1f1c087eb7
Blastmaster Fixup & Kleanup Crew <191918158+Blastmaster-Fixup-and-Kleanup-Crew@users.noreply.github.com>
2026-09-25 08:47:53 -0400

    Rewind HAL file to previous state

--- hal/halarm/omap3/halinit_up.c
+++ hal/halarm/omap3/halinit_up.c
@@ -3,33 +3,29 @@
  * LICENSE:         BSD - See COPYING.ARM in the top level directory
  * FILE:            hal/halarm/omap3/halinit_up.c
  * PURPOSE:         OMAP3 Board-Specific HAL Initialization
+ * PROGRAMMERS:     ReactOS Portable Systems Group
  */
+
+/* INCLUDES *******************************************************************/
 
 #include <hal.h>
 #define NDEBUG
 #include <debug.h>
 
+/* GLOBALS ********************************************************************/
+
+/* PRIVATE FUNCTIONS **********************************************************/
+
 VOID
 HalpInitPhase0(IN PLOADER_PARAMETER_BLOCK LoaderBlock)
 {
-    UNREFERENCED_PARAMETER(LoaderBlock);
 
-    HalpInitializeClock();
-    HalpInitializeInterrupts();
-
-    /*
-     * OMAP3 board init should configure:
-     * - timer device
-     * - UART/console
-     * - interrupt controller
-     */
 }
 
 VOID
 HalpInitPhase1(VOID)
 {
-    HalpInitializeDma();
-    HalpInitializeInterrupts();
+
 }
 
 /* EOF */
